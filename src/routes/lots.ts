3fd593129a87f0c519739a55e13543297e0bc8a7
lairaalmas <lairaalmas@gmail.com>
2026-04-16 20:16:56 -0300

    feature: create lot routes

--- src/routes/lots.ts
+++ src/routes/lots.ts
@@ -1,1 +1,7 @@
-export const lotsRoutes = () => {};
+import type { FastifyInstance } from 'fastify';
+import { getLots, getLotById } from '../controllers/lotsController.js';
+
+export const lotsRoutes = (app: FastifyInstance) => {
+  app.get('/', getLots);
+  app.get('/:id', getLotById);
+};
